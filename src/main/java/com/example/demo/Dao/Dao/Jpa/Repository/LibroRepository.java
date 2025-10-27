25ba7fd97735370d07fde9b0baa14bca838e1fe1
jean castro <castrojean848@gmail.com>
2025-10-27 12:15:14 -0500

    Refactor(Librorepository)
    Eliminar import sin funcion

--- src/main/java/com/example/demo/Dao/Dao/Jpa/Repository/LibroRepository.java
+++ src/main/java/com/example/demo/Dao/Dao/Jpa/Repository/LibroRepository.java
@@ -1,7 +1,7 @@
 package com.example.demo.Dao.Dao.Jpa.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import com.example.demo.Dao.Dao.Jpa.Entity.LibroEntity;
 
